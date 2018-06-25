689cacaad2096877228a1175995c872872ae60a0
Danny Marez <750danny@gmail.com>
2018-06-25 17:25:27 -0600

    added sql

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:gititdone@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
 
 tasks = []
 
@@ -9,10 +22,15 @@ tasks = []
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
-    
+    tasks = Task.query.all()
+
+
     return render_template('todos.html', title="Get It Done!", tasks=tasks)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
